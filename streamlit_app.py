4fdb7219fccf527912ef9a1ea8e2c1a32cdca98a
Gustavo Cunha <161786020+gckneip@users.noreply.github.com>
2025-07-31 20:13:29 +0000

    primeiras mudanças do site

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+# Page settings
+st.set_page_config(
+    page_title="GO - Concorrente",
+    page_icon="assets/go-icon.png",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "This app generates scripts for data clean rooms!"
+    }
 )
+
+# Set up main page
+col1, col2 = st.columns((6, 1))
+col1.title("GO Concorrente")
+col2.image("assets/golang-goper-logo.png", width=1000)
+st.sidebar.image("assets/GOLANG.png")
+action = st.sidebar.radio("Quer aprender a usar GO com concorrência?", ("Porque usar o GO para concorrência?",
+                                                                  "Goroutines",
+                                                                  "Channels",
+                                                                  "Runtime do GO"))
